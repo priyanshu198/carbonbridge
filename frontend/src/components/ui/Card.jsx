97234f136a5bd66f3a55681adc2998ea146df3e2
PRIYANSHU SINGH <57265530+priyanshu198@users.noreply.github.com>
2026-05-08 00:40:39 +0530

    Delete frontend directory

--- frontend/src/components/ui/Card.jsx
+++
@@ -1,4 +0,0 @@
-import { T } from "../../App.jsx";
-export default function Card({ children, style:sx={} }) {
-  return <div style={{background:T.bg2,border:`1px solid ${T.border}`,borderRadius:20,...sx}}>{children}</div>;
-}
